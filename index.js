c31966f0367670e0ebcc4445a96da23f76677b65
Mahrez Bendali <mahrez.bendali.ext@francetv.fr>
2022-12-19 21:09:13 +0100

    added keyboard input for letters, WIP complete word suggestions

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ let displayHangmanDiv = document.createElement("div");
 let displayWordDiv = document.createElement("div");
 let displayKeyboardDiv = document.createElement("div");
 let settingsDiv = document.createElement("div");
-let replayGameButton = document.createElement("button");
-let addWordButton = document.createElement("button");
+let wordTryButton = document.createElement("button");
+let wordTryInputDiv = document.createElement("div");
+let wordTryInputField = document.createElement("input");
+let wordTryInputSubmit = document.createElement("button");
+let replayGameButton = document.createElement("button"); // hide it when game status is fresh or ongoing
+let addWordButton = document.createElement("button"); // hide it when game status is fresh or ongoing
 
 // variables to handle JS game logic
-const Statuses = {
+const Status = {
   // fresh before first game, ongoing during a game, success on win, defeat on game over
   Fresh: "fresh",
   Ongoing: "ongoing",
@@ -26,7 +30,7 @@ let displayedWordArray = [];
 let alphabet = "abcdefghijklmnopqrstuvwxyz";
 let triesLeft = 7;
 let emptyLettersLeft = 0;
-let gameStatus = Statuses.Fresh;
+let gameStatus = Status.Fresh;
 
 // header
 header.textContent = "header";
@@ -41,8 +45,17 @@ document.body.appendChild(gameDiv);
 
 // settings zone
 settingsDiv.classList.add("settings");
+wordTryButton.textContent = "Proposer un mot";
 replayGameButton.textContent = "Rejouer";
 addWordButton.textContent = "Ajouter un mot";
+wordTryInputField.setAttribute("type", "text");
+wordTryInputSubmit.setAttribute("type", "submit");
+wordTryInputSubmit.textContent = "Ajouter";
+settingsDiv.appendChild(wordTryButton);
+wordTryInputDiv.appendChild(wordTryInputField);
+wordTryInputDiv.appendChild(wordTryInputSubmit);
+settingsDiv.appendChild(wordTryInputDiv);
+settingsDiv.appendChild(document.createElement("br"));
 settingsDiv.appendChild(replayGameButton);
 settingsDiv.appendChild(addWordButton);
 document.body.appendChild(settingsDiv);
@@ -58,9 +71,15 @@ function buildKeyboard() {
   }
 }
 
+function initSettings() {
+  wordTryInputField.style.display = "none";
+  wordTryInputSubmit.style.display = "none";
+}
+
 // inits all the elements of the game
 function initGame() {
-  gameStatus = Statuses.Ongoing;
+  gameStatus = Status.Ongoing;
+  initSettings();
   getRandomWord();
   initWordArray();
   displayWord();
@@ -89,7 +108,7 @@ function initWordArray() {
 
 // displays the current word with the missing letters
 function displayWord() {
-  displayWordDiv.textContent = displayedWordArray.join(""); // RegEx to find and remove every comma
+  displayWordDiv.textContent = displayedWordArray.join("");
 }
 
 // updates the game screen at each user input
@@ -137,7 +156,6 @@ function updateWordArray(letter) {
     console.log("lettre pas trouvée");
     triesLeft--;
   }
-
   console.log("current status of guess : " + displayedWordArray);
 }
 
@@ -148,13 +166,55 @@ function checkGameStatus() {
   for (let i = 0; i < displayedWordArray.length; i++) {
     if (displayedWordArray[i] == "_") emptyLettersLeft++;
   }
+
   console.log(
-    "empty letters left : " + emptyLettersLeft + ", tries left : " + triesLeft
+    "lettres manquantes : " +
+      emptyLettersLeft +
+      ", essais restants : " +
+      triesLeft
   );
+  if (emptyLettersLeft == 0) {
+    gameStatus = Status.Success;
+    gameOver();
+  }
+  if (triesLeft == 0) {
+    gameStatus = Status.Defeat;
+    gameOver();
+  }
 }
 
-// main logic "loop"
-initGame();
+function gameOver() {
+  displayEndGameMessage();
+  playAgain();
+}
+
+function displayEndGameMessage() {
+  let message;
+  if (gameStatus == Status.Success) {
+    message = "Félicitations ! Vous avez gagné ! Voulez-vous rejouer ?";
+  } else if (gameStatus == Status.Defeat) {
+    message = "Dommage, vous avez perdu =( Voulez-vous rejouer ?";
+  }
+  alert(message);
+  // make a new div to display an end game message based on gameStatut
+}
+
+function playAgain() {
+  // add button, on click we rebuild the game board
+}
+
+function isLetter(letter) {
+  for (let i = 0; i < alphabet.length; i++) {
+    if (alphabet[i] == letter) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function handleWordTry(word) {
+  console.log("letter " + word);
+}
 
 // handle a letter being clicked through the displayed keyboard
 displayKeyboardDiv.addEventListener("click", (e) => {
@@ -163,3 +223,27 @@ displayKeyboardDiv.addEventListener("click", (e) => {
   letterClicked.disabled = true;
   updateGameScreen(letterClicked.id);
 });
+
+// handle a letter being typed through the user's physical keyboard
+document.addEventListener("keyup", (e) => {
+  if (document.activeElement != wordTryInputField && isLetter(e.key)) {
+    let letterClicked = document.querySelector("#" + e.key);
+    letterClicked.classList.add("tried"); // disable the button and grey out the CSS
+    letterClicked.disabled = true;
+    updateGameScreen(letterClicked.id);
+  }
+});
+
+// handle the user wanting to submit a word
+wordTryButton.addEventListener("click", (e) => {
+  if (!wordTryInputDiv.hidden) {
+    wordTryInputDiv.style.display = "block";
+  }
+});
+
+wordTryInputSubmit.addEventListener("click", (e) => {
+  console.log(wordTryInputField.value);
+});
+
+// main logic "loop"
+initGame();
